c2c57e8a432f9fac66749a9a9caa6df00e2b4ff9
Brandon Lamb <blamb888@gmail.com>
2021-04-22 00:09:59 +0900

    add NPM axios for http POST request handling

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,35 @@
 // Put your application javascript here
+const axios = require('axios');
+
+console.log('metafields');
+
+let url = "https://brandon-lambs-store.myshopify.com/admin/products/bf39dff873081d5f3188b06656b3cbc1/metafields.json";
+
+console.log(url);
+
+// const metafield = JSON.stringify({
+//   "metafield": {
+//     "namespace": "global",
+//     "key": "test",
+//     "value": +1,
+//     "value_type": "integer"
+//   }
+// })
+
+
+const data = {
+  "metafield": {
+    "namespace": "global",
+    "key": "test",
+    "value": +1,
+    "value_type": "integer"
+  }
+};
+
+axios.post(url, data)
+    .then((res) => {
+        console.log(`Status: ${res.status}`);
+        console.log('Body: ', res.data);
+    }).catch((err) => {
+        console.error(err);
+    });

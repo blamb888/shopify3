134829e183c0c81e722a2eb486212e8473eff634
Brandon Lamb <blamb888@gmail.com>
2021-04-22 13:55:15 +0900

    make GET request to find current value of metafield

--- assets/application.js
+++ assets/application.js
@@ -7,19 +7,33 @@ let url = "https://bf39dff873081d5f3188b06656b3cbc1:shppa_494791fedd9fa764b23969
 
 console.log(url);
 
-const data = {
-  "metafield": {
-    "namespace": "global",
-    "key": "test",
-    "value": 1,
-    "value_type": "integer"
-  }
-};
+function createMetafield() {
 
-axios.post(url, data)
-    .then((res) => {
-        console.log(`Status: ${res.status}`);
-        console.log('Body: ', res.data);
-    }).catch((err) => {
-        console.error(err);
-    });
+  number = axios.get(url).then(response => console.log(response.data.metafields[0].value));
+
+  const data = {
+    "metafield": {
+      "namespace": "global",
+      "key": "test",
+      "value": number += 1,
+      "value_type": "integer"
+    }
+  };
+
+  axios.post(url, data)
+  .then((res) => {
+    console.log(`Status: ${res.status}`);
+    console.log('Body: ', res.data);
+  }).catch((err) => {
+    console.error(err);
+  });
+
+
+}
+
+// function axiosTest() {
+//      return axios.get(url).then(response => console.log(response.data.metafields[0].value));
+// }
+
+// createMetafield();
+axiosTest();

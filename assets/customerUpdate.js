69ba7a44f94e0e3172559279289cce0f6823ba01
brandon lamb <blamb888@gmail.com>
2021-06-15 09:48:45 +0900

    added more user variables, guest and registered. rendered more tests. script is functions ae.

--- assets/customerUpdate.js
+++ assets/customerUpdate.js
@@ -3,6 +3,8 @@ const API_KEY = "bf39dff873081d5f3188b06656b3cbc1";
 const SECRET = "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";
 const CUSTOMER_ID = "5328530178198";
 const CUSTOMER_ID2 = "5173186330774";
+const REGISTERED_USER = "5332411875478";
+const BOBBY_D = "5332394836118";
 const SHOP_NAME = 'brandon-lambs-store'
 
 console.log('🛠️ Updating customer marketing opt-in state...');
@@ -19,7 +21,7 @@ console.log('🛠️ Printing customer information...');
 
 async function customerUpdate() {
   shopify.customer
-    .update(CUSTOMER_ID2, { accepts_marketing: "false" })
+    .update(CUSTOMER_ID, { accepts_marketing: "false" })
     .then((customer) => console.log(customer))
     .catch((err) => console.error(err));
 

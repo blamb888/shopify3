45343ee58d790575a5d85edd695d6490bec80def
Brandon Lamb <blamb888@gmail.com>
2021-07-13 16:39:11 +0900

    make sepearte function for createTimestamp()

--- assets/createCustomer.js
+++ assets/createCustomer.js
@@ -17,22 +17,8 @@ const BOBBY_D_EMAIL = process.env.BOBBY_D_EMAIL;
 const RU_EMAIL = process.env.RU_EMAIL;
 
 async function createCustomer() {
-let date_ob = new Date();
-// adjust 0 before single digit date
-let date = ("0" + date_ob.getDate()).slice(-2);
-// current month
-let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-// current year
-let year = date_ob.getFullYear();
-// current hours
-let hours = date_ob.getHours();
-// current minutes
-let minutes = date_ob.getMinutes();
-// current seconds
-let seconds = date_ob.getSeconds();
-// prints date & time in YYYY-MM-DD HH:MM:SS format
-let timestamp = year + "/" + month + "/" + date + " " + hours + ":" + minutes + ":" + seconds;
-console.log(timestamp);
+
+  let timestamp = createTimestamp();
 
   let customer_search = await shopify.customer
     .search( {email: BRANDON_EMAIL_FS} )
@@ -55,6 +41,27 @@ console.log(timestamp);
     console.log(customer_search);
   }
 
+}
+
+
+function createTimestamp () {
+  let date_ob = new Date();
+  // adjust 0 before single digit date
+  let date = ("0" + date_ob.getDate()).slice(-2);
+  // current month
+  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+  // current year
+  let year = date_ob.getFullYear();
+  // current hours
+  let hours = date_ob.getHours();
+  // current minutes
+  let minutes = date_ob.getMinutes();
+  // current seconds
+  let seconds = date_ob.getSeconds();
+  // prints date & time in YYYY-MM-DD HH:MM:SS format
+  let timestamp = year + "/" + month + "/" + date + " " + hours + ":" + minutes + ":" + seconds;
+  console.log(timestamp);
+  return timestamp
 }
 
 createCustomer();

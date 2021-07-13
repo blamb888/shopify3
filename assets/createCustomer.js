a1deb9cc1deeee596f827b337a633ef425060f7e
Brandon Lamb <blamb888@gmail.com>
2021-07-13 17:10:41 +0900

    finalize createCustomer() with customerUpdate()

--- assets/createCustomer.js
+++ assets/createCustomer.js
@@ -10,35 +10,37 @@ const shopify = new Shopify({
   password: process.env.SECRET
 });
 
-const YOSHIE_EMAIL = process.env.YOSHIE_EMAIL;
-const BRANDON_EMAIL = process.env.BRANDON_EMAIL;
-const BRANDON_EMAIL_FS = process.env.BRANDON_EMAIL_FS;
-const BOBBY_D_EMAIL = process.env.BOBBY_D_EMAIL;
-const RU_EMAIL = process.env.RU_EMAIL;
-
-async function createCustomer() {
+async function createCustomer(customer_email) {
 
   let timestamp = createTimestamp();
 
   let customer_search = await shopify.customer
-    .search( {email: BRANDON_EMAIL_FS} )
+    .search( {email: customer_email} )
     .catch((err) => console.error(err));
-
-  console.log(customer_search);
 
   if(customer_search.length === 0) {
     console.log("customer search came up nill")
     let new_customer = await shopify.customer
       .create({
-        email: BRANDON_EMAIL_FS,
+        email: customer_email,
         tags: [ "MailUnsubscribe", `${timestamp}` ]
       })
       .catch((err) => console.error(err));
     console.log("new customer created")
     console.log(new_customer);
+    let customer_id = new_customer.id
+    console.log(customer_id);
   } else {
     console.log("customer search returned this:")
     console.log(customer_search);
+    let customer_id = customer_search[0].id;
+    console.log(customer_id)
+
+      let updated_customer = await shopify.customer
+        .update(customer_id, { accepts_marketing: "false" })
+        .catch((err) => console.error(err));
+        console.log("this is the update method");
+        console.log(updated_customer);
   }
 
 }
@@ -64,4 +66,13 @@ function createTimestamp () {
   return timestamp
 }
 
-createCustomer();
+
+const YOSHIE_EMAIL = process.env.YOSHIE_EMAIL;
+const BRANDON_EMAIL = process.env.BRANDON_EMAIL;
+const BRANDON_EMAIL_FS = process.env.BRANDON_EMAIL_FS;
+const BOBBY_D_EMAIL = process.env.BOBBY_D_EMAIL;
+const RU_EMAIL = process.env.RU_EMAIL;
+
+createCustomer('brandon+to@flagship.cc');
+
+module.exports = { createCustomer, createTimestamp }
--- assets/customerUpdate.js
+++ assets/customerUpdate.js
@@ -4,6 +4,11 @@ const Shopify = require('shopify-api-node');
 console.log('🛠️ Updating customer marketing opt-in state...');
 console.log('🛠️ Creating shopify variable...');
 
+const {
+  createCustomer,
+  createTimestamp
+} = require('./createCustomer');
+
 const shopify = new Shopify({
   shopName: process.env.SHOP_NAME,
   apiKey: process.env.API_KEY,
@@ -33,46 +38,6 @@ async function customerUpdate() {
 
 //START------------------ UPDATED CODE WITH CUSTOMER CREATION
 
-  let customer = await shopify.customer
-    .search( {email: BRANDON_EMAIL_FS} )
-    .catch((err) => console.error(err));
-  console.log(customer);
-
-  if(customer[0] === []) {
-    let customer_id = customer[0].id;
-    console.log(customer_id);
-    console.log("this is the IF statement");
-      await shopify.customer
-        .update(customer_id, { accepts_marketing: "false" })
-        .catch((err) => console.error(err));
-        console.log("this is the update method");
-        console.log(customer);
-  } else {
-    let new_customer = await shopify.customer
-      .create({
-        customer: {
-          email: BRANDON_EMAIL_FS,
-          first_name: "",
-          last_name: "",
-          metafields: [
-            {
-              key: "MailUnsubscribe",
-              value: true,
-              value_type: "boolean",
-              namespace: "global"
-            }
-          ]
-        }
-      })
-      .catch((err) => console.error(err));
-      console.log(new_customer.id);
-        await shopify.customer
-          .update(new_customer.id, { accepts_marketing: "false" })
-          .catch((err) => console.error(err));
-          console.log("this is the update method");
-          console.log(new_customer);
-      console.log("this is the else statement.");
-  }
 
 //END ------------------ UPDATED CODE WITH CUSTOMER CREATION
 

7c792167c293578d79b6cb40e4218e3bded25351
Brandon Lamb <blamb888@gmail.com>
2021-07-15 13:41:56 +0900

    took customer_search out of the array for easier access to values

--- assets/createCustomer.js
+++ assets/createCustomer.js
@@ -18,9 +18,10 @@ async function createCustomer(customer_email) {
 
   const timestamp = createTimestamp();
 
-  const customer_search = await shopify.customer
+  const customer_search_array = await shopify.customer
     .search( {email: customer_email} )
     .catch((err) => console.error(err));
+  const customer_search = customer_search_array[0];
 
   if(customer_search.length === 0) {
     console.log("customer search came up nill")
@@ -33,14 +34,14 @@ async function createCustomer(customer_email) {
     console.log("new customer created")
     console.log(new_customer);
     const customer_id = new_customer.id
-    console.log(customer_id);
+    console.log("This is the customer ID from the if: " + customer_id);
   } else {
     console.log("customer search returned this:")
     console.log(customer_search);
-    const customer_id = customer_search[0].id;
-    console.log(customer_id);
+    const customer_id = customer_search.id;
+    console.log("This is the customer ID from the Else: "+ customer_id);
       // Grab existing tags from customer_search object
-      const existing_tags = customer_search[0].tags;
+      const existing_tags = customer_search.tags;
       console.log("This is the existing_tags var: " + existing_tags);
       // Add existing tags to tags array in update method
       const updated_customer = await shopify.customer
